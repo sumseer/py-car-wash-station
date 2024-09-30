c1c2b4607152c32bb5e17dd4db40f4ffe45e8fa1
Max Savenya <savenya2013@gmail.com>
2024-09-30 19:18:51 +0300

    Solution updated

--- app/main.py
+++ app/main.py
@@ -1,21 +1,20 @@
 class Car:
-    def __init__(self,
-                 comfort_class: int,
-                 clean_mark: int,
-                 brand: str
-                 ) -> None:
+    def __init__(
+        self, comfort_class: int, clean_mark: int, brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self,
-                 distance_from_city_center: float,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int
-                 ) -> None:
+    def __init__(
+        self,
+        distance_from_city_center: float,
+        clean_power: int,
+        average_rating: float,
+        count_of_ratings: int,
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = round(average_rating, 1)
@@ -25,18 +24,16 @@ class CarWashStation:
         total_income = 0.0
         for car in cars:
             if car.clean_mark < self.clean_power:
-                price = self.calculate_washing_price(car)
-                total_income += price
+                total_income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
         return round(total_income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        clean_power_diff = self.clean_power - car.clean_mark
-        comfort_class = car.comfort_class
-        rating = self.average_rating
-        distance = self.distance_from_city_center
-
-        price = (comfort_class * clean_power_diff * rating) / distance
+        price = (
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
+            * self.average_rating
+        ) / self.distance_from_city_center
         return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
